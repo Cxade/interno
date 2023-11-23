20be748abba84ca4ba2d0c942748cc150b810be9
Cxade <cxade@mail.ru>
2023-11-23 19:07:58 +0500

    some improvements
    Parts of the pages have been moved to components, the code has been improved.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <header class="header">
+    <HeaderBlock />
+    <!-- <header class="header">
       <div class="header__main center">
         <router-link to="/" class="header__logo">
           <svg
@@ -8,15 +9,12 @@
             width="34"
             height="34"
             viewBox="0 0 34 34"
-            fill="none"
-          >
+            fill="none">
             <path
               d="M0 34.0003H13.4588V24.499C13.4588 22.4853 15.0898 20.8543 17.1035 20.8543C19.1172 20.8543 20.7482 22.4853 20.7482 24.499V34.0003H33.9975V0C15.2211 0 0 15.2211 0 34.0003Z"
-              fill="#CDA274"
-            />
+              fill="#CDA274" />
           </svg>
           <h2 class="header__logo-text">Interno</h2>
-          <!-- <a href="index.html" class="header__logo-text">Interno</a> -->
         </router-link>
         <nav class="navigation__header">
           <router-link to="/" class="navigation__link">Home</router-link>
@@ -24,9 +22,10 @@
           <router-link to="/blog" class="navigation__link">Blog</router-link>
         </nav>
       </div>
-    </header>
+    </header> -->
     <router-view />
-    <footer class="footer">
+    <FooterBlock />
+    <!-- <footer class="footer">
       <div class="footer__main center">
         <div class="footer__left">
           <router-link to="/" class="header__logo">
@@ -35,12 +34,10 @@
               width="34"
               height="34"
               viewBox="0 0 34 34"
-              fill="none"
-            >
+              fill="none">
               <path
                 d="M0 34.0003H13.4588V24.499C13.4588 22.4853 15.0898 20.8543 17.1035 20.8543C19.1172 20.8543 20.7482 22.4853 20.7482 24.499V34.0003H33.9975V0C15.2211 0 0 15.2211 0 34.0003Z"
-                fill="#CDA274"
-              />
+                fill="#CDA274" />
             </svg>
             <h2 to="/" class="header__logo-text">Interno</h2>
           </router-link>
@@ -82,107 +79,119 @@
           <p class="footer__right-text">(123) 456 - 7890</p>
         </div>
       </div>
-    </footer>
+    </footer> -->
   </div>
 </template>
 
+<script>
+import HeaderBlock from "@/blocks/HeaderBlock.vue";
+import FooterBlock from "@/blocks/FooterBlock.vue";
+
+export default {
+  components: {
+    HeaderBlock,
+    FooterBlock,
+  },
+};
+</script>
+
 <style lang="scss" scoped>
-@import "@/assets/styles/variables.scss";
+// @import "@/assets/styles/variables.scss";
 
-.navigation {
-  &__link {
-    color: $secondColor;
-    text-align: center;
-    font-size: 20px;
-    font-weight: 400;
-    line-height: 300%;
-    transition: color 0.1s;
+// .navigation {
+//   &__link {
+//     color: $secondColor;
+//     text-align: center;
+//     font-size: 20px;
+//     font-weight: 400;
+//     line-height: 300%;
+//     transition: color 0.1s;
 
-    &:hover {
-      color: $mainColor;
-    }
-  }
-  &__header {
-    display: flex;
-    gap: 50px;
-  }
-  &__footer {
-    display: flex;
-    flex-direction: column;
-  }
-}
-.header {
-  &__main {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding: 53px 0 57px 0;
-  }
+//     &:hover {
+//       color: $mainColor;
+//     }
+//   }
+//   &__header {
+//     display: flex;
+//     gap: 50px;
+//   }
+//   &__footer {
+//     display: flex;
+//     flex-direction: column;
+//   }
+// }
+// .header {
+//   &__main {
+//     display: flex;
+//     align-items: center;
+//     justify-content: space-between;
+//     padding: 53px 0 57px 0;
+//   }
 
-  &__logo {
-    display: flex;
-    align-items: baseline;
-    gap: 10px;
-  }
+//   &__logo {
+//     display: flex;
+//     align-items: baseline;
+//     gap: 10px;
+//   }
 
-  &__logo-text {
-    color: $secondColor;
-    font-size: 40px;
-    line-height: 125%;
-  }
-}
-.footer {
-  &__main {
-    display: flex;
-    margin-bottom: 134px;
-  }
-  &__left {
-    max-width: 394px;
-    margin-right: 101px;
+//   &__logo-text {
+//     color: $secondColor;
+//     font-size: 40px;
+//     line-height: 125%;
+//   }
+// }
+// .footer {
+//   &__main {
+//     display: flex;
+//     margin-bottom: 134px;
+//   }
+//   &__left {
+//     max-width: 394px;
+//     margin-right: 101px;
 
-    &-text {
-      color: $textColor;
-      font-size: 22px;
-      letter-spacing: 0.22px;
-      margin-top: 18px;
-      margin-bottom: 30px;
-    }
-    &-social {
-      display: flex;
-      gap: 54px;
-    }
-  }
+//     &-text {
+//       color: $textColor;
+//       font-size: 22px;
+//       letter-spacing: 0.22px;
+//       margin-top: 18px;
+//       margin-bottom: 30px;
+//     }
+//     &-social {
+//       display: flex;
+//       gap: 54px;
+//     }
+//   }
 
-  &__mid {
-    display: flex;
-    flex-direction: column;
-    max-width: 125px;
-    margin-right: 322px;
+//   &__mid {
+//     display: flex;
+//     flex-direction: column;
+//     max-width: 125px;
+//     margin-right: 322px;
 
-    &-heading {
-      color: $secondColor;
-      font-size: 25px;
-      margin-bottom: 9px;
-    }
-  }
+//     &-heading {
+//       color: $secondColor;
+//       font-size: 25px;
+//       margin-bottom: 9px;
+//     }
+//   }
 
-  &__right {
-    max-width: 258px;
-    &-title {
-      color: $secondColor;
-      font-size: 25px;
-      margin-bottom: 26px;
-    }
+//   &__right {
+//     max-width: 258px;
+//     &-title {
+//       color: $secondColor;
+//       font-size: 25px;
+//       margin-bottom: 26px;
+//     }
 
-    &-text {
-      color: $textColor;
-      font-size: 22px;
-      letter-spacing: 0.22px;
-    }
+//     &-text {
+//       color: $textColor;
+//       font-size: 22px;
+//       letter-spacing: 0.22px;
+//     }
 
-    &-mail {
-      line-height: 300%;
-    }
-  }
-}
+//     &-mail {
+//       line-height: 300%;
+//     }
+//   }
+// }
 </style>
---
+++ src/blocks/HeaderBlock.vue
@@ -0,0 +1,32 @@
+<template>
+  <header class="header">
+    <div class="header__main center">
+      <LogoMain />
+      <NavigationComp :style="{ gap: '50px' }" />
+    </div>
+  </header>
+</template>
+<script>
+import LogoMain from "@/components/LogoMain.vue";
+import NavigationComp from "@/components/NavigationComp.vue";
+
+export default {
+  name: "HeaderBlock",
+  components: {
+    LogoMain,
+    NavigationComp,
+  },
+};
+</script>
+<style lang="scss" scoped>
+@import "@/assets/styles/variables.scss";
+
+.header {
+  &__main {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 53px 0 57px 0;
+  }
+}
+</style>
